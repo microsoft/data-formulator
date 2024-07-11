1c86cc7816894b3fd1f01d1f5817432f92fbebdf
Dan Marshall <danmar@microsoft.com>
2024-07-11 15:08:38 -0700

    add module declarations

--- src/jpeg.d.ts
+++ src/jpeg.d.ts
@@ -6,3 +6,9 @@ declare module "*.jpg" {
     const content: any;
     export default content;
 }
+
+declare module "prettier";
+
+declare module "prettier/parser-babel";
+
+declare module "vm-browserify";
