00f5b9a9573157a251ac183be1d061699d8387a0
Dan Marshall <danmar@microsoft.com>
2024-11-25 18:18:20 -0800

    move filespec to script

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,6 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   {
     languageOptions: { globals: globals.browser },
     settings: {
