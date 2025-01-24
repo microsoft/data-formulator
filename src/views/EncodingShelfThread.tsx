668f9a492b3a20497d9961bfe7e03afc1cc5686c
Dan Marshall <danmar@microsoft.com>
2025-01-24 08:51:44 -0800

    refactor: use const for shuffled in getRandomSubarray function

--- src/views/EncodingShelfThread.tsx
+++ src/views/EncodingShelfThread.tsx
@@ -94,7 +94,8 @@ export const ChartElementFC: FC<{chart: Chart, tableRows: any[], boxWidth?: numb
             return x - Math.floor(x);
         }
         const getRandomSubarray = (arr: any[], size: number) => {
-            let shuffled = arr.slice(0), i = arr.length, temp, index;
+            const shuffled = arr.slice(0);
+            let i = arr.length, temp, index;
             while (i--) {
                 index = Math.floor((i + 1) * getRandom(233 * i + 888));
                 temp = shuffled[index];
