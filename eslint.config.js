1b90cf22df5df779722d8903037f38979d397786
Dan Marshall <danmar@microsoft.com>
2025-01-24 08:32:55 -0800

    add eslint-plugin-jsx-a11y for improved accessibility linting

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import jsxA11y from "eslint-plugin-jsx-a11y";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -18,6 +18,9 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
+    plugins: {
+      'jsx-a11y': jsxA11y,
+    },
     rules: {
       "@typescript-eslint/ban-ts-comment": "off",
       "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
@@ -33,6 +36,7 @@ export default [
       "prefer-const": "off",
       "react/jsx-key": "off",
       "react/no-unescaped-entities": "off",
+      "jsx-a11y/alt-text": "off",
     }
   }
 ];
