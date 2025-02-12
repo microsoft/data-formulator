b57c475acb235f17c1e7324237c666141fb9e0b5
Chenglong Wang <chenglong.wang@microsoft.com>
2025-02-12 13:36:23 -0800

    fix html escape bug

--- py-src/data_formulator/app.py
+++ py-src/data_formulator/app.py
@@ -54,13 +54,13 @@ CORS(app)
 
 def get_client(model_config):
     for key in model_config:
-        model_config[key] = html.escape(model_config[key].strip())
+        model_config[key] = model_config[key].strip()
 
     client = Client(
         model_config["endpoint"],
         model_config["model"],
         model_config["api_key"] if "api_key" in model_config else None,
-        model_config["api_base"] if "api_base" in model_config else None,
+        html.escape(model_config["api_base"]) if "api_base" in model_config else None,
         model_config["api_version"] if "api_version" in model_config else None)
 
     return client
