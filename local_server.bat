f5c954147628eef68ac57d846be62185eb9eb1b6
iafine <iafineden@gmail.com>
2025-03-18 17:36:44 +0800

    Add proxy in local_server script(#129)

--- local_server.bat
+++ local_server.bat
@@ -2,6 +2,10 @@
 :: Licensed under the MIT License.
 
 @echo off
+:: Set HTTP proxy environment variables only (Flask doesn't support SOCKS5)
+:: set http_proxy=http://127.0.0.1:7890
+:: set https_proxy=http://127.0.0.1:7890
+
 set FLASK_APP=py-src/data_formulator/app.py
 set FLASK_RUN_PORT=5000
 set FLASK_RUN_HOST=0.0.0.0
--- local_server.sh
+++ local_server.sh
@@ -1,4 +1,8 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
+# Set HTTP proxy environment variables only (Flask doesn't support SOCKS5)
+# export http_proxy=http://127.0.0.1:7890
+# export https_proxy=http://127.0.0.1:7890
+
 env FLASK_APP=py-src/data_formulator/app.py FLASK_RUN_PORT=5000 FLASK_RUN_HOST=0.0.0.0 flask run
